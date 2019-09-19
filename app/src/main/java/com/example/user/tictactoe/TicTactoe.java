356fc32a2d44b7a010f0658c08bcafb8a845faf6
Korablin1222 <kor4blin.m@yandex.com>
2019-09-19 17:50:12 +0300

    init project

---
+++ app/src/main/java/com/example/user/tictactoe/TicTactoe.java
@@ -0,0 +1,13 @@
+package com.example.user.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TicTactoe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tic_tactoe);
+    }
+}
